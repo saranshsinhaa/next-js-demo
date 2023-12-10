7c76da9fa5bb223686030994742ee18918f290cc
GauriChapra <gaurichapra12@gmail.com>
2023-12-10 23:32:12 +0530

    message

---
+++ pages/docs.js
@@ -0,0 +1,16 @@
+const Docs = () => {
+    return (
+     <>
+     <div>
+     <h1 className="text-4xl border-white rounded p-[20px] mr-3 border-2">Docs</h1>
+     </div>
+
+     <p className="text-2xl">this is a para</p>
+
+     <p className="text-[46px]">this is a para</p>
+
+     </> 
+    )
+};
+
+export default Docs;
