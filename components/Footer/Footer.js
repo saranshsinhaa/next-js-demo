004c186b65470696443ec4f687b305c45af81656
Akshat Agarwal <akshataga20@gmail.com>
2023-12-19 00:46:54 +0530

    hogaya

---
+++ components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p className="text-4xl">This is the footer.</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a>Home</a>
+        </li>
+        <li>
+          <a>About</a>
+        </li>
+        <li>
+          <a>Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
