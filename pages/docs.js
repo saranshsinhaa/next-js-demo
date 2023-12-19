cddc54fe43f79907120181c410028688ab36e807
Anushka Priyadarshi <anushka@Anushkas-MacBook-Air-2.local>
2023-12-19 11:54:29 +0530

    fixes

--- pages/docs.js
+++ pages/docs.js
@@ -4,7 +4,7 @@ const Docs = () => {
   return (
     <>
       <div className="my-6">
-        <Navbar />
+        
       </div>
       <div className="border-white rounded lg:p-7 p-2 border-2">
         <h1 className="text-5xl">Docs</h1>
@@ -15,7 +15,7 @@ const Docs = () => {
       </p>
       <p>Discover and deploy boilerplate example Next.js&nbsp;projects.</p>
       <div className="my-6">
-        <Footer />
+       
       </div>
     </>
   );
